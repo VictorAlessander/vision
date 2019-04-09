95586367494ef8eee7c9cfd94560af1b82338d2f
Victor Alessander <alessander@protonmail.com>
2019-04-08 22:23:59 -0300

    Creation of Register component and some constants

---
+++ src/constants/BillingManager.js
@@ -0,0 +1,7 @@
+export const BILLING_MANAGER_BASE_PATH = 'http://localhost:5000/'
+export const BILLING_MANAGER_DEBITS_PATH = 'debits/'
+export const BILLING_MANAGER_CATEGORIES_PATH = 'categories/'
+export const BILLING_MANAGER_REGISTRATION_PATH = 'registration'
+export const BILLING_MANAGER_LOGIN_PATH = 'login/'
+export const BILLING_MANAGER_LOGOUT_PATH = 'logout/access/'
+export const BILLING_MANAGER_USERS_PATH = 'users/'
